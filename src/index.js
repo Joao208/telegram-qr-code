1ec7defb8ef7533415e8c8a31f49280690c7ac46
João Augusto <joao.augusto@foodzilla.com.br>
2021-10-10 13:27:38 -0300

    feat: adding inline query

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
 const logger = require("js-logger");
+const { v4 } = require("uuid");
 
 logger.useDefaults();
 
@@ -9,14 +10,14 @@ const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+const baseUrl =
+  "https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=";
+
 bot.onText(/\/generate (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
 
-  const baseUrl =
-    "https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=";
-
-  logger.info(`Generate new qr code ${resp}`);
+  logger.info(`Generate new qr code ${resp}...`);
 
   bot.sendPhoto(chatId, baseUrl + resp);
 });
@@ -24,10 +25,19 @@ bot.onText(/\/generate (.+)/, (msg, match) => {
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  logger.info(`Starting new convrsation`);
+  logger.info(`Starting new convrsation...`);
 
   bot.sendMessage(
     chatId,
     "Para começar envie /generate com a url, numero ou o que deseja codificar, como por exemplo: /generate https://google.com.br"
   );
+});
+
+bot.on("inline_query", ({ query, id }) => {
+  const url = baseUrl + query;
+  const result = [{ type: "photo", id: v4(), photo_url: url, thumb_url: url }];
+
+  logger.info(`Inline query for text ${query}...`);
+
+  bot.answerInlineQuery(id, result);
 });
